2c2d8f437a926b6167222013c15da713a22c6791
Eric Storey <estorey@gmail.com>
2019-03-08 21:10:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,5 +6,5 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name 
-  puts "Welcome, #{name}. You are number #{katz_deli.get_"
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end 
